b82d3b1aad5162836789c8070d0296d9ce30869e
LingKangli <lkl@lkldeimac.local>
2016-03-01 15:26:55 +0800

    Initial Commit

---
+++ ElectricPro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElectricPro
+//
+//  Created by LingKangli on 16/3/1.
+//  Copyright © 2016年 com.TongChang.Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ElectricProTests/ElectricProTests.m
@@ -0,0 +1,39 @@
+//
+//  ElectricProTests.m
+//  ElectricProTests
+//
+//  Created by LingKangli on 16/3/1.
+//  Copyright © 2016年 com.TongChang.Co., Ltd. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ElectricProTests : XCTestCase
+
+@end
+
+@implementation ElectricProTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
